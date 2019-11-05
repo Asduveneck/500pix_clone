d91c59bdaedc98e5a405f1ecb014d80397f76ac8
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 14:06:25 -0800

    Fixes find_by_cred parameter: username -> user_name

--- thousand_pix/app/controllers/api/sessions_controller.rb
+++ thousand_pix/app/controllers/api/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(
-      params[:user][:username],
+      params[:user][:user_name],
       params[:user][:password]
     )
 
