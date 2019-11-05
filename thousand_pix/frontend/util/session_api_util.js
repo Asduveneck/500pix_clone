f117d3933881d042b44441944f9ae01e3bbc09f8
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 14:48:13 -0800

    Removes console.log tests for success/fail on session methods

--- thousand_pix/frontend/util/session_api_util.js
+++ thousand_pix/frontend/util/session_api_util.js
@@ -3,20 +3,14 @@ export const login = user => (
     method: 'POST',
     url: '/api/session',
     data: { user }
-  }).then( 
-    () => console.log("success logging in"),
-    () => console.log("failed to log in")
-  )
+  })
 );
 
 export const logout = () => (
   $.ajax({
     method: 'DELETE',
     url: '/api/session'
-  }).then(
-    () => console.log("success logging out"),
-    () => console.log("failed to log out")
-  )
+  })
 );
 
 export const signup = user => (
@@ -24,8 +18,5 @@ export const signup = user => (
     method: 'POST',
     url: '/api/users',
     data: { user }
-  }).then(
-    () => console.log("success signing up"),
-    () => console.log("failed to sign up")
-  )
+  })
 );
