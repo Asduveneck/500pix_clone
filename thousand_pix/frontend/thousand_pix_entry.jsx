f73fda6dcc9bb9e68032fbb61764cca4cc468df5
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 14:11:48 -0800

    restores logging out route test; logging out successful.

--- thousand_pix/frontend/thousand_pix_entry.jsx
+++ thousand_pix/frontend/thousand_pix_entry.jsx
@@ -10,4 +10,74 @@ import ReactDOM from 'react-dom';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render( <h1>1000 Pix entry file in 'frontend'. </h1>, root);
+
+  // Worked
+  // $.ajax({
+  //   method: 'post', 
+  //   url: '/api/users', 
+  //   data: {
+  //       user: {
+  //         user_name: "user_01",
+  //         password: 'password',
+  //         email: 'user_01_email',
+  //         first_name: 'firstname_01', // violates not null constraint?
+  //         last_name: 'lastname_01', 
+  //       }
+  //     }
+  // })
+  //   .then(() => console.log('success'),
+  //     () => console.log('you done goofed')
+  //   );
+  // $.ajax({
+  //   method: 'post', 
+  //   url: '/api/users', 
+  //   data: {
+  //       user: {
+  //         user_name: "user_02",
+  //         password: 'password',
+  //         email: 'user_02_email',
+  //         first_name: 'firstname_02', // violates not null constraint?
+  //         last_name: 'lastname_02', 
+  //       }
+  //     }
+  // })
+  //   .then(() => console.log('success'),
+  //     () => console.log('you done goofed')
+  //   );
+
+  // // Login 
+  // $.ajax({
+  //   method: 'post', 
+  //   url: '/api/session', 
+  //   data: {
+  //       user: {
+  //         user_name: "user_02",
+  //         password: 'password',
+  //         // email: 'user_02_email',
+  //         // first_name: 'firstname_02', // violates not null constraint?
+  //         // last_name: 'lastname_02', 
+  //       }
+  //     }
+  // })
+  //   .then(() => console.log('success logging in'),
+  //     () => console.log('you done goofed')
+  //   );
+  // Log 
+  $.ajax({
+    method: 'delete', 
+    url: '/api/session', 
+    // data: {
+    //     user: {
+    //       user_name: "user_02",
+    //       password: 'password',
+    //       // email: 'user_02_email',
+    //       // first_name: 'firstname_02', // violates not null constraint?
+    //       // last_name: 'lastname_02', 
+    //     }
+    //   }
+  })
+    .then(() => console.log('success logging out'),
+      () => console.log('you done goofed')
+    );
+
 })
