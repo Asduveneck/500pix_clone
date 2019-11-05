cd48dc5d37a2f31dd82f8466c66deb8d51da9fd0
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 13:20:46 -0800

    Tests routes via ajax call to create a user

--- thousand_pix/frontend/thousand_pix_entry.jsx
+++ thousand_pix/frontend/thousand_pix_entry.jsx
@@ -10,4 +10,24 @@ import ReactDOM from 'react-dom';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render( <h1>1000 Pix entry file in 'frontend'. </h1>, root);
+
+
+  $.ajax({
+    method: 'post', 
+    url: '/api/users', 
+    data: {
+        user: {
+          user_name: "user_01",
+          password: 'password',
+          email: 'user_01_email',
+          first_name: 'firstname_01', // violates not null constraint?
+          last_name: 'lastname_01', 
+        }
+      }
+  })
+    .then(() => console.log('success'),
+      () => console.log('you done goofed')
+    );
+  // .catch(()=>console.log('you done goofed ')); // no worky
+
 })
