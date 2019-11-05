3e7b9de315dae603703c7d415bcd58bd31a59387
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 15:27:41 -0800

    fixes commenting

--- thousand_pix/frontend/reducers/entities_reducer.js
+++ thousand_pix/frontend/reducers/entities_reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 // Imports other reducers from this directory to combine (and thereby shape our state)
 
-import users from './users_reducer'; //usersReducer imported as users for implicit kv
+import users from './users_reducer'; //usersReducer imported as users for implicit kv in JS
 
 export default combineReducers({
   users, // equivalent to users: users,
