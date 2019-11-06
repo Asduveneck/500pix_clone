58ec9e52706cb263a8537d980cc0009f9413cf96
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 16:23:40 -0800

    Adds getState and dispatch to test our store

--- thousand_pix/frontend/thousand_pix_entry.jsx
+++ thousand_pix/frontend/thousand_pix_entry.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 // Components to make:
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 
 // Components to Test on Window:
 
@@ -12,8 +12,13 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore(); // imports store 
+
   ReactDOM.render( <h1>1000 Pix entry file in 'frontend'. </h1>, root);
 
+  // Window Testing: 
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
 })
 
