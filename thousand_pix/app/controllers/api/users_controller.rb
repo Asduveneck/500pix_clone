41254674aef4675040b4b8dbe894a8ed58c0b820
Asduveneck <asduveneck@ucdavis.edu>
2019-11-06 15:46:09 -0800

    Adds, validates, and permits the new email column

--- thousand_pix/app/controllers/api/users_controller.rb
+++ thousand_pix/app/controllers/api/users_controller.rb
@@ -14,7 +14,7 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :password, :first_name, :last_name) 
+    params.require(:user).permit(:user_name, :password, :first_name, :last_name, :email) 
   end
 
 end
--- thousand_pix/app/models/user.rb
+++ thousand_pix/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :user_name, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  validates :first_name, :last_name, presence: true # non user auth
+  validates :first_name, :last_name, :email, presence: true # non user auth
 
   after_initialize :ensure_session_token
 
