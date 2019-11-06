6af1f937b5b22c1ef597cd8c02f98359cce19240
Asduveneck <asduveneck@ucdavis.edu>
2019-11-06 10:07:39 -0800

    Imports and renders the app, root, and store properly; removes superfluous imports

--- thousand_pix/frontend/thousand_pix_entry.jsx
+++ thousand_pix/frontend/thousand_pix_entry.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Components to make:
-// import Root from './components/root';
+import Root from './components/root';
 import configureStore from './store/store';
-import { signup, login, logout } from './actions/session_actions'
+
 
 
 
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const store = configureStore(); // imports store 
 
-  ReactDOM.render( <h1>1000 Pix entry file in 'frontend'. </h1>, root);
+  ReactDOM.render( <Root store={store}/> , root);
 
 })
 
