4f7f3aafbaa69f7ec027c8a6f806c2abd1deec54
Asduveneck <asduveneck@ucdavis.edu>
2019-11-06 11:57:27 -0800

    Fixes up by rendering the container I made and imports the routes

--- thousand_pix/frontend/components/App.jsx
+++ thousand_pix/frontend/components/App.jsx
@@ -1,10 +1,18 @@
+// Package dependencies
 import React from "react";
+import { Route } from 'react-router-dom'
+
+// Dependencies within this project (containers)
+import GreetingContainer from './greeting/greeting_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
+
 
 const App = () => (
   <div>
     <header>
       <h2> Within the App File </h2>
-
+      <GreetingContainer />
     </header>
 
     {/* Routes to use */}
