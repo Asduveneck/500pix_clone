9cfd8cee57f197dfa16850d31f2588efc788f297
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 16:18:12 -0800

    Adds root_reducer w/ a commented out state of slice I currently have

---
+++ thousand_pix/frontend/reducers/root_reducer.js
@@ -0,0 +1,43 @@
+import { combineReducers } from 'redux';
+
+
+/*
+We will import:
+  1. entities_reducer, which imports:
+    users_reducer 
+
+  2. session_reducer, which does not import anything
+  
+  3. errors_reducer, which imports:
+    session_error_reducer
+*/
+
+// Start importing all of your reducers, but check sample state too! 
+
+import entities from './entities_reducer';
+import session from './session_reducer'; 
+import errors from './errors_reducer'; 
+
+const rootReducer = combineReducers({
+  entities,
+  session,
+  errors,
+});
+
+export default rootReducer;
+
+/*
+Current state slice:
+  {
+    entities: {
+      users: {}
+    },
+    session: {
+      id: null
+    },
+    errors: {
+      session: []
+    }
+  }
+
+*/
