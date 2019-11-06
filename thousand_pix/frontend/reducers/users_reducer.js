a20f164686a44c6fdf23d4f6e5674994d02be7cd
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 16:54:43 -0800

    Adds testing to entry point; testing logout realized my users reducer does not account for logging out the user

--- thousand_pix/frontend/reducers/users_reducer.js
+++ thousand_pix/frontend/reducers/users_reducer.js
@@ -6,6 +6,7 @@ const usersReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+    // case logout user! 
     default:
       return state;
   }
--- thousand_pix/frontend/thousand_pix_entry.jsx
+++ thousand_pix/frontend/thousand_pix_entry.jsx
@@ -5,8 +5,7 @@ import ReactDOM from 'react-dom';
 // Components to make:
 // import Root from './components/root';
 import configureStore from './store/store';
-
-// Components to Test on Window:
+import { signup, login, logout } from './actions/session_actions'
 
 
 
@@ -16,9 +15,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
   ReactDOM.render( <h1>1000 Pix entry file in 'frontend'. </h1>, root);
 
-  // Window Testing: 
+  // Window Testing
+
+  // Testing Store
   window.getState = store.getState;
   window.dispatch = store.dispatch;
+
+  // Testing Thunk actions and User Auth
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+
+  // Constants for testing login logout! 
+    // Positive Control
+    window.user1 = {  // Already exists from previous tests
+      user_name: "user_01",
+      password: 'password',
+
+    };
+    // Negative Controls:
+    window.user2 = {
+      user_name: "user_02",
+      password: 'invalid password'
+  };
+
+  // Testing Signup Route Ajax method in session_util
+  window.user4 = {  // test
+    user_name: "user_04",
+    password: 'password',
+    email: 'user_04_email',
+    first_name: 'firstname_04',
+    last_name: 'lastname_04',
+  };
 
 })
 
