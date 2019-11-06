cff4fc31460e3d89d78cad8b6cf905d9657825fa
Asduveneck <asduveneck@ucdavis.edu>
2019-11-05 17:12:06 -0800

    Refactors reducer to contain a nextState; adds a case to logout the user in this reducer

--- thousand_pix/frontend/reducers/users_reducer.js
+++ thousand_pix/frontend/reducers/users_reducer.js
@@ -1,12 +1,16 @@
-import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
 
 // Keeps track of users... 
 const usersReducer = (state = {}, action) => {
   Object.freeze(state);
-  switch (action.type) {
+  let nextState = Object.assign({}, state);
+  
+  switch (action.type) { 
     case RECEIVE_CURRENT_USER:
-      return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
-    // case logout user! 
+      nextState[action.currentUser.id] = action.currentUser;
+      return nextState;
+    case LOGOUT_CURRENT_USER:
+      return {};
     default:
       return state;
   }
