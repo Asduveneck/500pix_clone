b907ba215502b335a5fc57c1b1780b4e880bb373
Asduveneck <asduveneck@ucdavis.edu>
2019-11-06 18:03:17 -0800

    It'd be nice to split the sign up form as two seperate pages with a successful refactor. Stashing for now

---
+++ thousand_pix/frontend/components/session_form/home_feed_setup_form/hf_form_container.jsx
@@ -0,0 +1,4 @@
+// I may need to modify the forms since the forms are different... 
+// Will put off towards later because I need to look into activeRecord '.exists' if I make these two seperate forms. 
+// I'll be better off with one long form and one shorter form for now. 
+
