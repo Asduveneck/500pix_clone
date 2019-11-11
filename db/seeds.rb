662e80754f327284c31028fb065ce5323b95a070
Asduveneck <asduveneck@ucdavis.edu>
2019-11-11 00:23:02 -0800

    Wipes and seeds the data only if everything works.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ActiveRecord:: BaseTransaction do 
 
-demo = User.create(username: 'guest', password: 'NotPassword', first_name: 'Guest', last_name: 'User'. email: 'demo_user@1000pix.com' )
+  # Clear Tables:
+  User.destroy_all 
+
+
+  # Seeds:
+  demo = User.create(username: 'guest', password: 'NotPassword', first_name: 'Guest', last_name: 'User'. email: 'demo_user@1000pix.com' )
+
+end
