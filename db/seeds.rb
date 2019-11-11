5ede70cdfd6580f480a7da3080036ecbaa63ef68
Asduveneck <asduveneck@ucdavis.edu>
2019-11-11 00:19:27 -0800

    Seeds a demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+demo = User.create(username: 'guest', password: 'NotPassword', first_name: 'Guest', last_name: 'User'. email: 'demo_user@1000pix.com' )
