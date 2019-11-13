59284ffab3e815072c43526743f9c1c2b49ddd14
Asduveneck <asduveneck@ucdavis.edu>
2019-11-13 13:32:23 -0800

    Completes and imports photo errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import session from './session_errors_reducer';
+import photos from './photo_errors_reducer';
 
 // Tracks any error messages
 const allErrorsReducer = combineReducers({
-  session
+  session,
+  photos,
 });
 
 export default allErrorsReducer;
