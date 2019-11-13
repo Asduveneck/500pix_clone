f4226d166e13f670cc47de205ea5f1d10035f59b
Asduveneck <asduveneck@ucdavis.edu>
2019-11-12 21:49:47 -0800

    Generates controllers for photos and categories; defines placeholders for controller methods

---
+++ app/controllers/api/photos_controller.rb
@@ -0,0 +1,24 @@
+class Api::PhotosController < ApplicationController
+  
+  def index  
+  end
+
+  def new
+  end
+
+  def create
+  end
+    
+  def show
+  end
+  
+  def edit 
+  end 
+
+  def update 
+  end
+
+  def destroy 
+  end
+  
+end
