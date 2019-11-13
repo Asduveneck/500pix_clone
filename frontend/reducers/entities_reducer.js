849348ae64b07f4057df8df1c570d29fa6786adc
Asduveneck <asduveneck@ucdavis.edu>
2019-11-13 14:41:17 -0800

    Completes photo reducer and imports it into entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,7 +3,8 @@ import { combineReducers } from 'redux';
 // Imports other reducers from this directory to combine (and thereby shape our state)
 
 import users from './users_reducer'; //usersReducer imported as users for implicit kv in JS
-
+import photos from './photos_reducer';
 export default combineReducers({
   users, // equivalent to users: users,
+  photos,
 });
---
+++ frontend/reducers/photos_reducer.js
@@ -0,0 +1,31 @@
+import merge from 'lodash/merge';
+import {
+  RECEIVE_PHOTO,
+  RECEIVE_PHOTOS,
+  DELETE_PHOTO,
+} from '../actions/photo_actions'
+
+const _defaultSession = Object.freeze({});
+
+// Default state would be... no pictures?
+
+// Handles shape for all the photos
+
+const photosReducer = (state = _defaultSession, action) => {
+  Object.freeze(state);
+  let nextState = Object.assign({}, state)
+  switch (action.type) {
+    case RECEIVE_PHOTO:
+      nextState = merge(nextState, [action.photo.id]: action.photo)
+      return nextState;
+    case RECEIVE_PHOTOS:
+      return action.photos;
+    case DELETE_PHOTO:
+      delete nextState(action.photoId);
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default photosReducer;
