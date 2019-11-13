d3b1b37d65df6dcdd35d3b820e3807ed71ce3d37
Asduveneck <asduveneck@ucdavis.edu>
2019-11-13 00:16:36 -0800

    Updates code to somewhat match video; need to test individually now

--- frontend/components/index_page/index_page.jsx
+++ frontend/components/index_page/index_page.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
-class SessionForm extends React.Component {
+class IndexPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      photos: [] // Following along
+    }
+
+    // Binding Functions
+    this.fetchPhotos = this.fetchPhotos.bind(this);
   }
 
+  fetchPhotos() { // Ripping off tutorial vid https://vimeo.com/278727054 
+    // For now... Will need to do all of this in a reducer LATER!
+    $.ajax({
+      url: `/api/photos`
+    }).then(photos => {
+      // debugger;
+      this.setState({photos})
+    })
+  }
+
+  componentDidMount() {
+    this.fetchPhotos()
+  }
+
+
+
   render() {
+    if(!this.state.photos) return null; // 
     return(
       <div className="index_page_page">
 
@@ -14,3 +37,5 @@ class SessionForm extends React.Component {
   }
 
 }
+
+export default IndexPage;
