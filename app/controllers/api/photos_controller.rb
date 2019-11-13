f7b65f66ebb00a2fb8a2b760740bb656b16479e5
Asduveneck <asduveneck@ucdavis.edu>
2019-11-12 23:37:04 -0800

    Defines index, create, and show controller methods. Time to test showing via AWS

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,15 +1,25 @@
 class Api::PhotosController < ApplicationController
   
-  def index  
+  def index
+    @photos = Photo.all
+    render @photos # we want infinite scroll so probably not yet...
   end
-
-  def new
+    
+  def new # form to get or post a photo
   end
 
   def create
+    @photo = Photo.new (photo_params)
+    if @photo.save
+      render :show   # Could also be the edit form right after too...
+    else
+      render json: @photo.full_messages, status: 400 # :bad_request, but could be anything else too...
+    end
   end
     
   def show
+    @ohoto = Photo.find(params[:id])
+    render :show
   end
   
   def edit 
@@ -20,5 +30,15 @@ class Api::PhotosController < ApplicationController
 
   def destroy 
   end
-  
+
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :description, # user input
+      :user_id, # from current user
+      :views, :rating # not user inputted
+    )
+  end
+
 end
