38ff91498533bc8e1ac885e440d19f1a1724c349
Asduveneck <asduveneck@ucdavis.edu>
2019-12-10 10:36:35 -0800

    Comment out placeholder top

--- frontend/components/index_page/index_indv_photo.jsx
+++ frontend/components/index_page/index_indv_photo.jsx
@@ -21,7 +21,7 @@ class IndexIndvPhoto extends React.Component{
       <div className="indvPhoto" style={nonHover}>
         <div className="i_p_gradient">
           <div className="top">
-            <h1>Placeholder Top</h1>
+            {/* <h1>Placeholder Top</h1> */}
           </div>
           <div className="bottom">
             <h4>{title}</h4> 
