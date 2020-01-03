a3b3d57874db69ae21612f59f86f806147bbe779
Asduveneck <asduveneck@ucdavis.edu>
2020-01-02 16:26:12 -0800

    Individual photo show in progress

---
+++ frontend/components/photo_show/photo_show_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+
+const mSTP = ({ session, entities: { users } }) => ({
+
+});
+
+//mapDispatchToProps
+const mDTP = dispatch => ({
+
+});
+
+export default connect(mSTP, mDTP)(NavBar)
