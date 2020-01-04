f84f0d4e5a658cab1c5057f5411aa64e2e5e4c7a
Asduveneck <asduveneck@ucdavis.edu>
2020-01-03 17:47:50 -0800

    Goal for inf scroll

--- frontend/components/infinitescroll.js
+++ frontend/components/infinitescroll.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// Goal: Maybe make this generic for galleries? Or plan for that so in the future we can reuse this component elsewhere!
 class InfiniteScroll extends React.Component {
   render() {
     return <div />
