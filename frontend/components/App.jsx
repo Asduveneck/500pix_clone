129435e8ebe6193dc50e95a2ff03946f96f70125
Asduveneck <asduveneck@ucdavis.edu>
2020-01-03 23:27:01 -0800

    Import component for displaying photo

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -12,6 +12,7 @@ import LoginFormContainer from './session_form/login_form_container';
 import SplashPageContainer from './splash_page/splash_page_container';
 import NavBarContainer from './nav_bars/nav_bar_container'; 
 import IndexPageContainer from './index_page/index_page_container';
+import photoShowContainer from './photo_show/photo_show_container';
 
 const App = () => (
   <div className="entireApp_file"> {/* This wraps around entire page. Background white */}
@@ -23,6 +24,7 @@ const App = () => (
       <br />
     {/* Routes to use */}
       <ProtectedRoute exact path="/index" component={IndexPageContainer} />
+      <ProtectedRoute path="/photo/" component={photoShowContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute path="/signup" component={SignupFormContainer} />
   </div>
