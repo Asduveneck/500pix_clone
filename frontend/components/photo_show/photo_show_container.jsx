d3a5c57a7995237a64505327d73863ccc153aacd
Asduveneck <asduveneck@ucdavis.edu>
2020-01-04 18:18:11 -0800

    Container to fetch photos

--- frontend/components/photo_show/photo_show_container.jsx
+++ frontend/components/photo_show/photo_show_container.jsx
@@ -1,12 +1,17 @@
 import { connect } from 'react-redux';
 import photoShow from './photo_show';
-const mSTP = (state) => ({
-  test: "placeholder"
-});
+import { fetchPhoto } from '../../actions/photo_actions';
+
+
+const mSTP = (state, ownProps) => {
+  console.log("in mSTP");
+  // debugger;
+  return { photo: state.entities.photos[ownProps.match.params.photoId],}
+};
 
 //mapDispatchToProps
 const mDTP = dispatch => ({
-
+  fetchPhoto: photoId => dispatch(fetchPhoto(photoId)),
 });
 
 export default connect(mSTP, mDTP)(photoShow)
