c198aba2cb7486d76c9f001da16ca5b64c57cc27
Asduveneck <asduveneck@ucdavis.edu>
2020-01-05 23:59:37 -0800

    Basic lifecycle methods for user profile page

--- frontend/components/user_profile/user_show_container.jsx
+++ frontend/components/user_profile/user_show_container.jsx
@@ -1,14 +1,16 @@
 import { connect } from 'react-redux';
 import userShow from './user_show';
-// import {  } from '../../actions/session_actions'; // time to make user actions
+import { fetchUser, clearUser } from '../../actions/user_actions'; // time to make user actions
 
-
+// Modify this and App.jsx so that we match by username
+// Will probably need to update our AJAX call too.
 const mSTP = (state, ownProps) => ({
-  // photo: state.entities.photos[ownProps.match.params.photoId],
+  user: state.entities.users[ownProps.match.params.userId], // make sure this is some other user
 });
 
 const mDTP = dispatch => ({
-  // fetchPhoto: photoId => dispatch(fetchPhoto(photoId)),
+  fetchUser: userId => dispatch(fetchUser(userId)),
+  clearUser: () => dispatch(clearUser()),
 });
 
 export default connect(mSTP, mDTP)(userShow)
