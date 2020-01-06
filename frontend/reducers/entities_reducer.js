787d6fd57e4cb8faff055a2452432eddc321a142
Asduveneck <asduveneck@ucdavis.edu>
2020-01-05 22:40:59 -0800

    Rename user in user slice of state to current_user for clarity and to distinguish when we start viewing other users

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,9 +2,9 @@ import { combineReducers } from 'redux';
 
 // Imports other reducers from this directory to combine (and thereby shape our state)
 
-import users from './users_reducer'; //usersReducer imported as users for implicit kv in JS
+import currentUser from './current_user_reducer'; //usersReducer imported as users for implicit kv in JS
 import photos from './photos_reducer';
 export default combineReducers({
-  users, // equivalent to users: users,
+  currentUser, // equivalent to users: users,
   photos,
 });
