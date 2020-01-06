9dd5a1398818cede459750234f4e00baf5faedaa
Asduveneck <asduveneck@ucdavis.edu>
2020-01-06 01:26:45 -0800

    comment in todo for user photo

--- frontend/components/user_profile/user_show_container.jsx
+++ frontend/components/user_profile/user_show_container.jsx
@@ -2,8 +2,9 @@ import { connect } from 'react-redux';
 import userShow from './user_show';
 import { fetchUser, clearUser } from '../../actions/user_actions'; // time to make user actions
 
-// Modify this and App.jsx so that we match by username
-// Will probably need to update our AJAX call too.
+// TODO: Modify this and App.jsx so that we match by username
+    // Will probably need to update our AJAX call too.
+// TODO: import fetchPhoto or fetchPhotos so we can show off the users images
 const mSTP = (state, ownProps) => ({
   user: state.entities.users[ownProps.match.params.userId], // make sure this is some other user
 });
