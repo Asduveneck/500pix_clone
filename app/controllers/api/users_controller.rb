01b851b2ec89b83c0cd82cf44a0fd35a1782de16
Asduveneck <asduveneck@ucdavis.edu>
2020-01-05 17:55:35 -0800

    define show and update methods for users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,10 +11,29 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def update 
+    @user = self.current_user # calls helper method from application controller.
+    if @user.update_attributes(user_updatable_params)
+      remder :show
+    else
+      render json: @user.errors.full_messages, status: 400
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:user_name, :password, :first_name, :last_name, :email) 
   end
+
+  def user_updatable_params
+    params.require(:user).permit(:first_name, :last_name, :location_city, :location_country, :about, :website, :instagram_username, :facebook_address, :twitter) 
+  end
+
 
 end
