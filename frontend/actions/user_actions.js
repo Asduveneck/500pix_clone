22c279371d55bc5636ea5bc3acd5e3105cebbfbe
Asduveneck <asduveneck@ucdavis.edu>
2020-01-05 20:50:16 -0800

    Define read update error actions for user

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,9 +1,33 @@
 // ======================== Imports and Exports ==========================
+import * as APIUtil from '../util/user_api_util';
+
+export const RECEIVE_USER = "RECEIVE_USER";
+export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
+// export const DELETE_USER = "DELETE_USER"; // no deleting users yet
 
 // ============================= Actions ======================================
 
 // ------- CRUD --------
+// Create not present since we make that in the session
+
+export const receiveUser = user => ({
+  type: RECEIVE_USER,
+  user
+})
+
+// Update: in THUNK.
+// Delete: no deleting users yet
 
 // --------- Errors --------
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_USER_ERRORS,
+  errors
+});
+
+export const clearUserErrors = () => ({
+  type: RECEIVE_PHOTO_ERRORS,
+  errors: [], // Return an empty array, wiping away errors.
+});
 
 // ========================== Thunk Actions  ===================================
