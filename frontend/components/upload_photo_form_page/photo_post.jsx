6d9bbe5f35167cc803e5006e88bf4afa2df6bc2c
Asduveneck <asduveneck@ucdavis.edu>
2020-01-07 22:01:29 -0800

    Redoes form from class to function

--- frontend/components/upload_photo_form_page/photo_post.jsx
+++ frontend/components/upload_photo_form_page/photo_post.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 
-class photoCreate extends React.Component {
+const photoCreate = () => {
 
-  constructor(props) {
-    super(props);
+  // constructor(props) { // only valid in react class... 
+  //   super(props);
+  //   this.handleFiles = this.handleFiles.bind(this)
+  // }
+
+  const handleFiles = () => {
+    console.log("test")
   }
 
-  render() {
+  return(
+    <div className="photoCreate_Page">
+      New photos here
+      <div className="drop-area">
+        <form className="photo_form">
 
+            <span>Upload photos</span>
 
-    return(
-      <div className="photoCreate_Page">
-        New photos here
+            <button>Select Photos </button>
+            <input type="file" id="fileElem" onChange={handleFiles()}/>
+            {/* files supposed to go in handleFiles */}
+          <span>Or drag and drop photos anywhere on this page</span>
+        </form>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 export default photoCreate;
