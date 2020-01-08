81923d5c032836d070bfa983e628a57be2e13d82
Asduveneck <asduveneck@ucdavis.edu>
2020-01-07 20:50:23 -0800

    Create photoCreate files with basic setup

---
+++ frontend/components/upload_photo_form_page/photo_post.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class photoCreate extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+
+    return(
+      <div className="photoCreate_Page">
+
+      </div>
+    )
+  }
+}
+export default photoCreate;
---
+++ frontend/components/upload_photo_form_page/photo_post_container.jsx
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import photoPost from './photo_post';
+import { createPhoto } from '../../actions/photo_actions';
+
+
+const mSTP = (state, ownProps) => ({
+  
+});
+
+const mDTP = dispatch => ({
+  createPhoto: photo => dispatch(createPhoto(photo))
+});
+
+export default connect(mSTP, mDTP)(photoShow)
