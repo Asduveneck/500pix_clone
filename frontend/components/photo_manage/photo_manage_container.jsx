3583becfc4ff7a35ee2ab94892ff5434d536d1be
Asduveneck <asduveneck@ucdavis.edu>
2020-01-20 23:28:41 -0800

    Import read update delete actions for photos and loads in current user id

---
+++ frontend/components/photo_manage/photo_manage_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import photoMange from './photo_manage';
+import { fetchPhoto, updatePhoto, deletePhoto } from '../../actions/photo_actions';
+
+
+const mSTP = ({ session, entities: { currentUser } }) => ({
+  currentUserID: currentUser[session.id].id
+});
+
+const mDTP = dispatch => ({
+  fetchPhoto: photoId => dispatch(fetchPhoto(photoId)),
+  updatePhoto: photo => dispatch(updatePhoto(photo)),
+  deletePhoto: photoId => dispatch(deletePhoto(photoId)),
+});
+
+export default connect(mSTP, mDTP)(photoManage)
