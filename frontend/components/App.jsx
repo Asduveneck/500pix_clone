418a212ae27f9830b7c329506c61dd7d51887206
Asduveneck <asduveneck@ucdavis.edu>
2020-01-20 23:24:33 -0800

    imports container for managing photos

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -14,7 +14,8 @@ import NavBarContainer from './nav_bars/nav_bar_container';
 import IndexPageContainer from './index_page/index_page_container';
 import PhotoShowContainer from './photo_show/photo_show_container';
 import UserShowContainer from './user_profile/user_show_container';
-import PhotoCreateContainer from './upload_photo_form_page/photo_post_container';
+import PhotoCreateContainer from './photo_upload/photo_post_container';
+import PhotoManageContainer from './photo_manage/photo_manage_container';
 
 const App = () => (
   <div className="entireApp_file"> {/* This wraps around entire page. Background white */}
@@ -28,6 +29,7 @@ const App = () => (
       <ProtectedRoute exact path="/index" component={IndexPageContainer} />
       <ProtectedRoute exact path="/photo/:photoId" component={PhotoShowContainer} /> {/* Make it so that this is a normal route, but components within are protected */}
       <ProtectedRoute exact path="/new/photo/" component={PhotoCreateContainer} />
+      <ProtectedRoute exact path="/manage_photos" component={PhotoManageContainer} />
       {/* <ProtectedRoute exact path="/users/:user_name" component={UserShowContainer}/>  Ideal */}
       <ProtectedRoute exact path="/users/:userId" component={UserShowContainer}/>
       <AuthRoute exact path="/login" component={LoginFormContainer} />
