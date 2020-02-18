f841b82bb1394aaad94e3dd5bd78b75e51b2fa85
Asduveneck <asduveneck@ucdavis.edu>
2020-02-17 23:51:42 -0800

    Redoes spacing, coloring, and puts in placeholder text. Should've wrote a function to generate this html...

--- frontend/components/user_profile/user_setting.jsx
+++ frontend/components/user_profile/user_setting.jsx
@@ -17,56 +17,64 @@ class UserSetting extends React.Component {
       <div className="usr_setting_page">
         <div className="header">
           {/* Contains image for cover photo and user picture */}
+          <div className="cover_photo">
+          </div>
+          <div className="user_photo">
+          </div>
         </div>
 
         <div className="row">
         <section>
             <label htmlFor="first_name">First Name</label>
-            <input type="text" name="First Name" id="first_name" className="first_name" />
+            <input type="text" name="First Name" placeholder="First Name" id="first_name" className="first_name input"/>
           </section>
 
           <section>
             <label htmlFor="last_name">Last Name</label>
-            <input type="text" name="Last Name" id="last_name" />
+            <input type="text" name="Last Name" placeholder="Last Name" id="last_name" className="last_name input"/>
           </section>
         </div>
 
+        <div className="pseudoLabel">Location</div>
         <div className="row">
           <section>
-            <label htmlFor="">Location</label>
-            <input type="text" name="" id="City" />
-            <input type="text" name="" id="Country" />
+            <label htmlFor="City" className="blank"> </label>
+            <input type="text" maxLength="128" name="City" placeholder="City" id="City" className="City input"/>
+          </section>
+          <section>
+            <label htmlFor="Country" className="blank"> </label>
+            <input type="text" maxLength="128" name="Country" placeholder="Country" id="Country" className="Country input"/>
           </section>
         </div>
 
         <div className="row">
           <section>
             <label>About (optional)</label>
-            <textarea name="About" id="about" cols="30" rows="10"></textarea>
+            <textarea name="About" id="about" cols="30" rows="10" className="input">Tell the world your story.</textarea>
           </section>
         </div>
         
         <div className="row">
           <section>
             <label htmlFor="website">Website</label>
-            <input type="text" name="Website" id="website" className="website" />
+            <input type="text" maxLength="128" name="Website" id="website" placeholder="Enter URL" className="website input"/>
           </section>
 
           <section>
             <label htmlFor="instagram">Instagram</label>
-            <input type="text" name="Instagram" id="instagram" className="instagram"/>
+            <input type="text" maxLength="128" name="Instagram" id="instagram" placeholder="Username" className="instagram input"/>
           </section>
         </div>
 
         <div className="row">
           <section>
             <label htmlFor="facebook">Facebook</label>
-            <input type="text" name="Facebook" id="facebook" className="facebook" />
+            <input type="text" maxLength="128" name="Facebook" id="facebook" placeholder="Full facebook address" className="facebook input"/>
           </section>
 
           <section>
             <label htmlFor="twitter">Twitter</label>
-            <input type="text" name="Twitter" id="twitter" className="twitter"/>
+            <input type="text" maxLength="128" name="Twitter" id="twitter" placeholder="Username" className="twitter input"/>
           </section>
         </div>
 
