c191aa791b69c2324b1f4b69f9559b84d23aeb09
Asduveneck <asduveneck@ucdavis.edu>
2020-02-18 12:30:54 -0800

    moves current user out of private method to be shared with children...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,15 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
-  private
+  
 
   def current_user
     return nil unless session[:session_token]
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
+  private
+  
   def logged_in?
     !!current_user
   end
